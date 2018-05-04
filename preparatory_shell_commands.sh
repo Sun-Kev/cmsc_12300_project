b6f392c66a53dd8564a9d32c855503ac35cbee06
tamos <tyler.amos@alumni.carleton.ca>
2018-05-04 10:01:28 -0500

    prep commands added

--- preparatory_shell_commands.sh
+++ preparatory_shell_commands.sh
@@ -4,14 +4,10 @@
 pip3 install -r requirements.txt
 
 
-# collect a list of all the geojson files, append filename 
+# activate and download aleppo - remember to export PL_API_KEY=[api key]
 
-# ls geojson_cities | grep ".geojson" > geojson_city_list.txt
-
-#echo "geojson_files= 'geojson_city_list.txt' # this made by the prep shell script" >> #settings.py
-
-
-
+bash activate.sh aleppoids.txt
+python3 PlanetDownloader.py download_links.txt aleppo, aleppo_geojson, aleppo_merged
 
 
 
